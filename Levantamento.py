555ae2217faf3a38ccc559fb36d30ed11d8d3c68
bsensix <109923359+bsensix@users.noreply.github.com>
2023-01-11 13:48:45 -0300

    Update Levantamento.py

--- Levantamento.py
+++ Levantamento.py
@@ -66,10 +66,6 @@ col1.title('Relatório Solos:')
 uploaded_files = col1.file_uploader("Upload Planilha de Solos 📥")
 
 
-col2.title('Relatório Drone:')
-# Upload Arquivo csv 
-uploaded_files_drone = col2.file_uploader("Upload Planilha de IVs 📥")
-
 tabela = pd.read_excel(uploaded_files)
 
 tabela_original = tabela
